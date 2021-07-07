7cf9967d7529778405e6f2a44fd3455951b8b17e
EllenHub <shirpalalona@gmail.com>
2021-07-07 14:33:43 +0300

    Redux classwork

--- src/index.js
+++ src/index.js
@@ -3,10 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
+const initialState = {
+    counterValue: 0,
+    posts: []
+}
+
+const reducer = (state =initialState,action)=>{
+    switch(action.type) {
+        case 'INC': {
+            return {...state, counterValue: state.counterValue + 1}
+        }
+        case 'SET_POSTS': {
+            return {...state, posts: action.payload}
+        }
+        default:
+            return state
+
+    }
+}
+const store = createStore(reducer)
 ReactDOM.render(
+
   <React.StrictMode>
-    <App />
+
+      <Provider store={store}>
+          <App/>
+      </Provider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
